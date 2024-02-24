33bf130281762b30e780955d18b49259f1c53cf3
gabydian <145726594+gabydian@users.noreply.github.com>
2024-02-23 21:56:56 -0300

    Update calculadoraLoja.js
    atualizado

--- calculadoraLoja.js
+++ calculadoraLoja.js
@@ -1,5 +1,6 @@
 //Faça uma função de nome calcularValorCompra que irá receber três parâmetros. O primeiro será um numero que representa o valor de uma compra. O segundo parâmetro é um boolean que representa se o usuário possui ou não convênio com o estabelecimento e o último parâmetro representa se a compra está sendo feita ou não com o cartão da loja. Se a pessoa que estiver fazendo a compra possuir o cartão da loja e possui convênio então o desconto será de 15%. Se o usuário possuir convenio ou estiver fazendo a compra pelo cartão da loja o desconto é de 10%. Caso o usuário não tenha nenhum desses benefícios não existe desconto para a compra. 
 
+const { deepEqual } = require('Node:assert');
 function calcularValorCompra (valorCompra, convenio, cartaoLoja) {
 
     if (cartaoLoja === true && convenio === true) {
@@ -10,7 +11,12 @@ function calcularValorCompra (valorCompra, convenio, cartaoLoja) {
     }
     return console.log(valorCompra);
 }
-calcularValorCompra (100, true, true);
-let valorCompra = 100;
-let convenio = true;
-let cartaoLoja = true;
+//calcularValorCompra (100, true, true);
+//let valorCompra = 100;
+//let convenio = true;
+//let cartaoLoja = true;
+
+deepEqual (100, calcularValorCompra(100, false, false));
+deepEqual (90, calcularValorCompra(100, false, true));
+deepEqual (90, calcularValorCompra(100, true, false));
+deepEqual (85, calcularValorCompra(100, true, true));
