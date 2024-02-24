f5829ecbdfc315353d0f3a09c095b45d2bada5be
gabydian <145726594+gabydian@users.noreply.github.com>
2024-02-23 22:22:57 -0300

    Add files via upload

---
+++ listaAnos.js
@@ -0,0 +1,12 @@
+//**Enunciado I:** Considere o array `anos` declarado acima. Faça uma função que irá retornar todos os anos que são menores que o ano atribuído como parâmetro. Você deve fazer o uso do `for` e a função deve retornar um novo array. 
+
+//**Enunciado II:** Considere o array `anos` declarado acima. Faça uma função que irá retornar a quantidade de anos que existem que são maiores ou igual ao ano passado como parâmetro.
+
+const anos = [Number('2012'),Number('2010'),Number('2020'),Number('2022'),Number('2035'), Number('2045')];
+
+function menoresAnos(year) {
+    const menoresAnos [];
+    for(let ano of anos) {
+        return new Array[year];
+    }
+}
