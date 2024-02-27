76bf606437fab69a9e2939175b4f4ffb24dc8c68
gabydian <145726594+gabydian@users.noreply.github.com>
2024-02-26 21:44:27 -0300

    Update listaAnos.js

--- listaAnos.js
+++ listaAnos.js
@@ -4,12 +4,34 @@
 
 const anos = [Number('2012'),Number('2010'),Number('2020'),Number('2022'),Number('2035'), Number('2045')];
 
-function menoresAnos(year) {
+function menoresAnos(año) {
     const menoresAnos [];
     for(let ano of anos) {
-        if(ano < anos){
+        if(ano < año){
             menoresAnos.push(ano)
         }
     }
     return menoresAnos;
 }
+
+function anosMaiores(año) {
+    const anosMaiores = []
+    for (let ano of anos) {
+        if(ano >= año){
+            anosMaiores.push(ano)
+        }
+    }
+    return anosMaiores;
+}
+function anosDecimais(año) {
+    const anosDecimos = []
+    for (let ano of anos){
+        if (ano % 10 === 0) {
+            anosDecimos.push(ano)
+        }
+    }
+    return anosDecimos;
+}
+console.log(anosMenores(2025));
+console.log(anosMaiores(2004));
+console.log(anosDecimos());
